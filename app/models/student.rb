51bba8b04b6a30ca533220afde1a41765216ec6d
MMLThomas <michael.thomas2014@gmail.com>
2020-07-27 18:17:02 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,21 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(query)
+    if query == '' 
+      Student.all 
+    else 
+      results = []
+      Student.all.each do |name| 
+        #byebug
+        if name.name.upcase.include?(query.upcase)
+          results << name 
+        end
+      end
+      results
+    end
+  end
+
 end
